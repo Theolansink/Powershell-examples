d9f80177a9c90e672f802764cfca8370e3bfef20
Theolansink <34719919+Theolansink@users.noreply.github.com>
2025-09-29 17:19:09 +0200

    winevents-foreach-example .ps1 added

---
+++ critical-events.ps1
@@ -0,0 +1,28 @@
+# --------------------------------------------
+# Example: High Severity Security Events
+# --------------------------------------------
+
+# Define the Event Log and filter for Security events for last 7 days
+$logName = "Security"
+
+# Define filter hash table for Get-WinEvent; critical events and errors from last 10 days.
+$filterHash = @{
+    LogName   = $logName
+    Level     = 1,2  # 1 = Critical, 2 = Error
+    StartTime = (Get-Date).AddDays(-10)
+}
+
+# Get high severity security events
+$highSeverityEvents = Get-WinEvent -FilterHashtable $filterHash -MaxEvents 50
+
+# Display the events
+$highSeverityEvents | ForEach-Object {
+    Write-Host "----------------------------------------"
+    Write-Host "Time: $($_.TimeCreated)"
+    Write-Host "ID: $($_.Id)"
+    Write-Host "Level: $($_.LevelDisplayName)"
+    Write-Host "Message: $($_.Message)"
+}
+
+
+
