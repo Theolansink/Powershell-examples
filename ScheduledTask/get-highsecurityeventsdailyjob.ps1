3b6f8cfccf5fafd36ad5ff3b72fa33430af6d6a1
Theolansink <34719919+Theolansink@users.noreply.github.com>
2025-10-08 09:49:57 +0200

    scheduleed job changed

--- ScheduledTask/get-highsecurityeventsdailyjob.ps1
+++ ScheduledTask/get-highsecurityeventsdailyjob.ps1
@@ -1,3 +1,6 @@
+# scheduled Job: werkt alleen in Powershell 5!!! 
+# Legacy. Vervangen door Schedulked Task in Powershell 7.
+
 # Maak een dagelijkse trigger om 18:00
 $trigger = New-JobTrigger -Daily -At 18:00
 
